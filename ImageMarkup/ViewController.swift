7f0a1f5c4f11a69eb703e83c1831e45358997b30
Matt Dias <matt.dias6@gmail.com>
2020-09-05 15:46:39 -0400

    quick and dirty UI

--- ImageMarkup/ViewController.swift
+++ ImageMarkup/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import Combine
+
+enum Segment: Int {
+    case zev = 0
+    case matt
+}
+
+enum Email: String {
+    case zev = "zev@zeveisenberg.com"
+    case matt = "mdiasdeveloper@gmail.com"
+}
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var calls: Set<AnyCancellable> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        getImage(for: .zev)
+    }
+    
+    deinit {
+        calls = []
     }
 
-
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        guard let segment = Segment(rawValue: sender.selectedSegmentIndex) else { return }
+        
+        getImage(for: segment)
+    }
+    
+    func getImage(for segment: Segment) {
+        var email: String
+        
+        switch segment {
+        case .zev:
+            email = Email.zev.rawValue
+        case .matt:
+            email = Email.matt.rawValue
+        }
+        
+        APIClient().get(API.gravatar(email: email).url())
+            .sink(receiveCompletion: { (completion) in
+                switch completion {
+                case let .failure(error):
+                    print("Couldn't get users: \(error)")
+                case .finished: break
+                }
+            }) { image in
+                self.imageView.image = image
+        }.store(in: &calls)
+    }
 }
 
