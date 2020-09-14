3d6709fcb9ef61c2753975c725f2772b7062b77c
Ryan Stone <ryan@ryanstone.io>
2020-09-13 20:28:56 -0400

    Comments

--- ImageMarkup/ViewController.swift
+++ ImageMarkup/ViewController.swift
@@ -43,28 +43,37 @@ class ViewController: UIViewController {
         client.get(API.subreddit.url()) // first, get the list of posts
             .decode(type: Response.self, decoder: JSONDecoder())
             .map(\.imageUrls) // get out the image URLs we care about, which are of the form (index: Int, url: URL)
-            .flatMap { (indicesAndURLs: [(Int, URL)]) in // turn each URL into a publisher of the corresponding image
-                indicesAndURLs.publisher // Getting a Publishers.Sequence from an Array is how you do that
-                    .flatMap { (index, url) in // URL -> publisher of resulting image
-                        client.get(url)
-                            .map(UIImage.init(data:))
-                            .replaceNil(with: UIImage(systemName: "photo")!)
-                            .replaceError(with: UIImage(systemName: "photo")!)
-                            .map { (index, $0) }
-                    }
-                    .setFailureType(to: Error.self) // outer pipeline has errors, so we need them too
-        }
-        .receive(on: DispatchQueue.main)
-        .sink(receiveCompletion: { completion in
-            if case .failure(let error) = completion {
-                print(error)
-            }
-        }, receiveValue: { value in
-            print(type(of: value))
-            // put image in model
-        })
-        .store(in: &cancellables)
+            .flatMap(self.getPublishersForURLS)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    print(error)
+                }
+            }, receiveValue: { value in
+                print(type(of: value))
+                // put image in model
+            })
+            .store(in: &cancellables)
     }
+
+  func getPublishersForURLS(_ indicesAndURLs: [(Int, URL)]) -> AnyPublisher<(Int, UIImage), Error> {
+    // [].publisher creates an Publisher of Publishers of the Elements in the array
+    return indicesAndURLs.publisher // Getting a Publishers.Sequence from an Array is how you do that
+      .flatMap(self.getURLLatIndex) // Take the success output of the publisher and returns a new publisher with different output
+      .setFailureType(to: Error.self)
+      .eraseToAnyPublisher() // outer pipeline has errors, so we need them too
+  }
+
+  func getURLLatIndex(index: Int, url: URL) -> AnyPublisher<(Int, UIImage), Never> {
+      // Combine makes retain cycles easy to create. If you know a stream will complete once it's done doing
+      // it's work capturing self is OK, but beware that you can create retain cycles with ease.
+      self.client.get(url)
+          .map(UIImage.init(data:))
+          .replaceNil(with: UIImage(systemName: "photo")!)
+          .replaceError(with: UIImage(systemName: "photo")!)
+          .map { (index, $0) }
+          .eraseToAnyPublisher()
+  }
 }
 
 extension MutableCollection {
